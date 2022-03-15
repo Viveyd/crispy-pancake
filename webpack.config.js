54b89cfc7967edc38111d36e1b723b050def731a
vivi <harhar.asuncion@gmail.com>
2022-03-15 19:33:26 +0800

    Finished setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "index.bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|jpeg|png|svg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
